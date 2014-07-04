bcec2bf9eeeb8c906c3c36737980b9d3d8505a4b
saintach <saintayechanhtoon@gmail.com>
2014-07-04 18:56:20 +0630

    Initial Commit for website

---
+++ index.php
@@ -0,0 +1,298 @@
+<!--
+Theme Name: Nova2
+Author: Saint Aye Chan Htoon
+Author URI: http://www.saintayechanhtoon.me
+Description: The Nova2 for simple/minimal resume website is integrated with Bootstrap 3.0 that looks great on any device.
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Saint Aye Chan Htoon</title>
+    <meta http-equiv="refresh" content="0; url=with-chris.php">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Hello, I am a web developer currently based in Singapore – freshly graduated and can't wait to experience the challenges of IT industry."/>
+    <!--Bootstrap-->
+    <link href="./css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <!--My stylesheet-->
+    <link href="./css/style.css" rel="stylesheet" media="screen">
+    <!--Lightbox-->
+    <link href="./css/lightbox.css" rel="stylesheet" media="all">
+    <!--Favicon-->
+    <link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+    <!--Google Font-->
+    <link href="http://fonts.googleapis.com/css?family=Alef|Raleway|Quattrocento:700" rel="stylesheet" type="text/css">
+    <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container">
+      <!--About-->
+      <section class="row about" id="top">
+        <div class="col-md-12">
+          <div class="row">
+            <div class="col-md-7 profile">
+              <h1><strong>SAINT AYE CHAN HTOON</strong></h1>
+                <article>
+                    <span><img src="./images/me.JPG"></span>
+                    <p>I am a soon-to-be graduate about to enter the exciting field of web and software engineering, and am ready to begin applying the skills I have learned to industry specific situations.</p>
+                    <p>For me software development is not only a job, but a passion. It comes to mind along with traveling, reading, and mint chocolate chip ice-cream. From the designing of graphics used for my own personal travel blog, to the problem solving process of analytical programming, I find outlets for creative expression in all stages of development.</p>
+                </article>
+            </div>
+            <div class="col-md-4 social">
+              <span>
+			  	<p><span class="ico" aria-hidden="true" data-icon="&#x21;"></span>Clementi West St 2, Singapore</p>	
+			  </span>
+			  <span>
+			  	<p><span class="ico" aria-hidden="true" data-icon="&#x22;"></span> +65 97901543</p>
+			  </span>
+			  <span>
+			  	<p><span class="ico" aria-hidden="true" data-icon="&#x23;"></span> <a class="link" href="http://saintayechanhtoon.me">www.saintayechanhtoon.me</a></p>
+			  </span>
+		  	  <span>
+			  	<p><span class="ico" aria-hidden="true" data-icon="&#x24;"></span> <a class="link" href="mailto:hello@saintayechanhtoon.me">hello@saintayechanhtoon.me</a></p>
+		  	  </span>
+		  	  <span>   		  	 
+        		<a href="https://github.com/saintach" class="ico1" aria-hidden="true" data-icon="&#x25;" title="GitHub" target="_blank"></a>
+        		<a href="http://sg.linkedin.com/pub/saint-aye-chan-htoon/37/179/bb7/" class="ico1" aria-hidden="true" data-icon="&#x26;" title="LinkedIn" target="_blank"></a>
+        		<a href="https://twitter.com/saintach" class="ico1" aria-hidden="true" data-icon="&#x27;" title="Twitter" target="_blank"></a>
+        		<a href="https://plus.google.com/u/0/108971505498275499979?rel=author" rel="author" class="ico1" aria-hidden="true" data-icon="&#x28;" title="Google Plus" target="_blank"></a>
+        		<a href="http://backpacksandroses.com" class="ico1" aria-hidden="true" data-icon="&#x29;" title="Wordpress" target="_blank"></a>
+		  	  </span>
+		  	  <a href="./resume-saint_aye_chan_htoon.doc" class="ico7" target="_top"><h5> Download Resume</h5> <span aria-hidden="true" data-icon="&#x2c;"></span></a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--Expertises-->
+      <section class="row sec">
+        <div class="col-md-12">
+          <div class="row">
+            <aside class="col-md-3 aside">
+              <h2>Expertise</h2>
+            </aside>
+            <div class="col-md-6">
+              <article class="border-left content">
+		  		<p>WEB TECHNOLOGIES:<b> PHP, HTML5, CSS3, JavaScript, JQuery, ASP.NET, Wordpress, Twitter Bootstrap</b></p>
+		  		<p>LANGUAGES:<b> C++, C#, Java</b></p>
+		  		<p>TOOLS/IDE:<b> Visual Studio 2012, Git, SourceTree, SVN, Coda2, Dreamweaver CS5, PhotoShop CS5, Lightwave 3D, InkScape</b> </p>
+		  		<p>RDBMS:<b> MSSQL, MySQL</b></p>
+		  		<p>PLATFORMS:<b> Windows, Mac, Linux</b></p>
+			  </article>
+            </div>
+            <div class="col-md-3">
+              <span><h4>{PHP}</h4></span>	
+              <span><h4>{CSS}</h4></span>
+              <span><h4>{HTML}</h4></span>
+              <span><h4>{C#}</h4></span>
+              <span><h4>{ASP .NET}</h4></span>
+			  <span><h4>{JS}</h4></span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--Experiences-->
+      <section class="row sec">
+        <div class="col-md-12">
+          <div class="row">
+            <aside class="col-md-3 aside">
+              <h2>Experiences</h2>
+            </aside>
+            <div class="col-md-6">
+              <article class="border-left content">
+		  		<h3>Wordpress Plug-in and Blog</h3>
+	  			<p><i>Current Project</i></p>
+	  			<p>Currently, designing a responsive wordpress theme for my own travel blog (available soon at www.backpacksandroses.com). Working on Wordpress route plug-in using Google Map API which will be contributed to Wordpress' open source community. Technology use: PHP, HTML, CSS, GoogleMap, JQuery, Bootstrap, Git, SourceTree.</p>
+			  	<p></p>
+			  </article>
+            </div>
+            <div class="col-md-3">
+              <h5 class="date">Dec 2012 -  current</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="row sec">
+        <div class="col-md-12">
+          <div class="row">
+            <aside class="col-md-3 aside">
+            </aside>
+            <div class="col-md-6">
+              <article class="border-left content">
+		  		<h3>Beleaguer Online Go Tournament (Final Year Project)</h3>
+	  			<p><i>University of Wollogong</i></p>
+	  			<p>Worked with team to produce an online multiplayer tournament game based on Go. Used project management tools and SVN. Built responsive user interface using modern standards and design techniques (wireframe skeleton, OOP, minimalism, intuitive). Game and website employs PHP, JavaScript, HTML5, MySQL, Twitter Bootstrap, as well as other standard technologies. Site will go live soon (available at www.dpsstudio.com).</p>
+			  	<p></p>
+			  </article>
+            </div>
+            <div class="col-md-3">
+              <h5 class="date">June 2013 -  Aug 2013</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="row sec">
+        <div class="col-md-12">
+          <div class="row">
+            <aside class="col-md-3 aside">
+            </aside>
+            <div class="col-md-6">
+              <article class="border-left content">
+		  		<h3>Freelance Web Designer </h3>
+	  			<p>Designed and deployed websites for small and medium sized businesses. 
+Managed website development through all stages, project scoping, contract negotiation, and delivery of final product.
+</p>
+			  	<p></p>
+			  </article>
+            </div>
+            <div class="col-md-3">
+              <h5 class="date">Dec 2012 - Jan 2013</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+       <section class="row sec">
+        <div class="col-md-12">
+          <div class="row">
+            <aside class="col-md-3 aside">
+            </aside>
+            <div class="col-md-6">
+              <article class="border-left content">
+		  		<h3>Intern</h3>
+	  			<p><i>NCS Pte Ltd</i></p>
+	  			<p>I was responsible for testing the new HDB car park payment web application; creating test plans, data integration testing, and UA testing. The web application was implemented in Java and used IBM DB2. </p>
+			  	<p></p>
+			  </article>
+            </div>
+            <div class="col-md-3">
+              <h5 class="date">Sep 2011 -  Feb 2012</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="row sec">
+        <div class="col-md-12">
+          <div class="row">
+            <aside class="col-md-3 aside">
+            </aside>
+            <div class="col-md-6">
+              <article class="border-left content">
+		  		<h3>ESPY Face Recognition Software (Final Year Project)</h3>
+	  			<p><i>Temasek Polytechnic</i></p>
+	  			<p>Had the majority of responsibility in the development of ESPY–an integrated desktop and web software solution using an open source face detection and recognition API. Software automates user privacy, reduces power consumption, and provides attendance utilities for educators. Designed with Face.com (previously open source API), WCF, ASP.NET, C#, and OpenCV.</p>
+			  	<p></p>
+			  </article>
+            </div>
+            <div class="col-md-3">
+              <h5 class="date">Jul 2011 -  Sep 2011</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!--Education-->
+        <section class="row sec">
+        <div class="col-md-12">
+          <div class="row">
+            <aside class="col-md-3 aside">
+              <h2>Education</h2>
+            </aside>
+            <div class="col-md-6">
+              <article class="border-left content">
+		  		<h3>Bachelor of Computer Science</h3>
+	  			<p><i>University of Wollogong, SIM '13</i></p>
+	  			<p>Course Hightlight: 3D Modeling and Animation, Computer Graphics, System Administration, Game Development, Game Engine Fundamental, Multimedia Computing</p>
+			  	</article>
+            </div>
+            <div class="col-md-3">
+              <h5 class="date">Oct 2012 -  Sep 2013</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+       <section class="row sec">
+        <div class="col-md-12">
+          <div class="row">
+            <aside class="col-md-3 aside">
+            </aside>
+            <div class="col-md-6">
+              <article class="border-left content">
+		  		<h3>Temasek Polytechnic</h3>
+	  			<p><i>Diploma in Information Technology '12</i></p>
+	  			<p>Course Hightlight: Communication in the Workplace, Client-Server Application Development, Object-Oriented Analysis and Design, Data Structures and Algorithms, Database Systems, Web Design, IT Project Management, Open Source Application Development</p>
+			  	</article>
+            </div>
+            <div class="col-md-3">
+              <h5 class="date">Apr 2009 -  Feb 2012</h5>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+      <!--Portfolio-->
+      <section class="row sec">
+        <div class="col-md-12">
+          <div class="row">
+            <aside class="col-md-3 aside">
+              <h2>Portfolio</h2>
+            </aside>
+            <div class="col-md-9">
+                <div class="row">
+                  <figure class="col-md-6 pic vertpan">
+                    <a href="images/portfolio/dpsstudio.png" data-lightbox="portfolio" Title="http://dpsstudio.com"><img src="images/portfolio/dpsstudio.png"></a>
+                    <figcaption><h5>DPSStudio website</h5> A marketing website made for final year project team. Clean design. As usual, a minimal design</figcaption>
+                  </figure>
+                  <figure class="col-md-6 pic shrink">
+                    <a href="images/portfolio/beleaguer-matches.png" data-lightbox="portfolio" Title="http://beleaguer.dpsstudio.com"><img src="images/portfolio/beleaguer-matches.png"></a>
+                    <figcaption><h5>Beleaguer Online GO Tournament</h5> Final Year Project @ SIM. An online Go tournament website coded in PHP, MySQL and JavaScript</figcaption>
+                  </figure>
+                </div>
+                <div class="row">
+                  <figure class="col-md-6 pic vertpan">
+                    <a href="images/portfolio/beleaguer-pets.png" data-lightbox="portfolio" Title="Beleaguer Responsive Website"><img src="images/portfolio/beleaguer-pets.png"></a>
+                    <a href="images/portfolio/beleaguer-signup.png" data-lightbox="portfolio" Title="Sign Up Page | Beleaguer Go Tournament" style="display:none;"></a>
+                    <a href="images/portfolio/beleaguer-login.png" data-lightbox="portfolio" Title="Login Page | Beleaguer Go Tournament" style="display:none;"></a>
+                    <figcaption><h5>Beleaguer Online GO Tournament</h5> Final Year Project @ SIM. An online Go tournament website coded in PHP, MySQL and JavaScript</figcaption>
+                  </figure>
+                  <figure class="col-md-6 pic shrink">
+                    <a href="images/portfolio/espy.jpg" data-lightbox="portfolio" Title="ESPY Face Recognition Software"><img src="images/portfolio/espy.jpg"></a>
+                    <figcaption><h5>Espy Face Recognition Software</h5> Final Year Project @ Temasek Polytechnic. An hybrid attendance system using facial recognition developed in C# and ASP.NET</figcaption>
+                  </figure>
+                </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      
+      <section class="row sec">
+        <div class="col-md-12 footer">
+          <center><a href="#top" class="up" aria-hidden="true" data-icon="&#x3b;" title="Up?"></a></center>
+          <center><h5>&copy; Saint Aye Chan Htoon 2013 </h5></center>
+          <center><h6>Icon font by Icomoon. Responsive theme by Bootstrap 3.0.</h6></center> 
+        </div>
+      </section>
+
+    </div>
+  </body>
+  <script src="./js/jquery-1.10.2.min.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
+  <script src="./js/lightbox-2.6.min.js"></script>
+  <script>
+    $('a').click(function(){
+      $('html, body, div').animate({
+        scrollTop: $($(this).attr('href') ).offset().top
+        }, 500);
+        return false;
+    });
+  </script>
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-43291400-1', 'saintayechanhtoon.me');
+      ga('send', 'pageview');
+    </script>
+</html>
---
+++ with-chris.php
@@ -0,0 +1,196 @@
+<!--
+Theme Name: Nova2
+Author: Saint Aye Chan Htoon
+Author URI: http://www.saintayechanhtoon.me
+Description: The Nova2 for simple/minimal resume website is integrated with Bootstrap 3.0 that looks great on any device.
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>With Chris</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Hello, I am a web developer currently based in Singapore – freshly graduated and can't wait to experience the challenges of IT industry."/>
+		<!--Bootstrap-->
+		<link href="./css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<!--My stylesheet-->
+		<link href="./css/style.css" rel="stylesheet" media="screen">
+		<!--Lightbox-->
+		<link href="./css/lightbox.css" rel="stylesheet" media="all">
+		<!--Favicon-->
+		<link href="favicon.ico" rel="shortcut icon" type="image/x-icon">
+		<!--Google Font-->
+		<link href="http://fonts.googleapis.com/css?family=Alef|Raleway|Quattrocento:700" rel="stylesheet" type="text/css">
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+	    <div class="container">
+	      <!--About-->
+			<section class="row about" id="top">
+				<div class="profile">
+					<div class="text-center">
+						<h1>Saint &amp; Chris Development Team</h1>
+					</div>
+					<div class="col-md-2">
+						<img class="image1" src="./images/me.JPG">
+						<span>   		  	 
+			        		<a href="https://github.com/saintach" class="ico2" aria-hidden="true" data-icon="&#x25;" title="GitHub" target="_blank"></a>
+			        		<a href="http://linkedin.com/in/saintayechanhtoon" class="ico2" aria-hidden="true" data-icon="&#x26;" title="LinkedIn" target="_blank"></a>
+			        		<a href="https://twitter.com/saintach" class="ico2" aria-hidden="true" data-icon="&#x27;" title="Twitter" target="_blank"></a>
+						</span>
+					</div>   
+					<div class="col-md-8 text-center">
+						<p><strong>Who are we?</strong></p>
+						<p>	Consider us a one stop solution for your internet marketing and technology-based business solution needs. 
+						Utilizing technology is increasingly important for business and individuals to effectively market themselves and interact with their target audience.
+						Whether you are looking for the first website for your business, hoping to maintain or update an existing code base, or are just looking for contractors
+						to provide support for an existing project, we are here to help.</p>
+						<p> We are a professional development team with the experience needed to give you confidence that your needs are satisfied with the most up-to-date and secure technology.
+						We develop websites and  mobile applications that are cross platform, secure, easy to access, and easy to find.
+						</p>
+						<p>
+						Our goal is to provide clear communication with our clients, so that they feel secure throughout the entire development process that they are getting exactly what they want.
+						</p>
+					</div>
+					<div class="col-md-2">
+						<img class="image2" src="./images/chris.jpg">
+					</div>
+				</div>
+			</section>
+	      <!--Expertises-->
+			<section class="row sec">
+				<div class="col-md-12">
+					<div class="row">
+						<aside class="col-md-3 aside">
+						<h2>What We Provide</h2>
+						</aside>
+						<div class="col-md-9">
+							<article class="border-left content">
+								<p><strong>THINGS WE DO</strong></p>
+								<ul>
+									<li>Full size, scalable websites.</li>
+									<li>Responsive user interface and pleasant user experience</li>
+									<li>Websites and Web Applications that work on mobile with any modern browser</li>
+									<li>Professional business websites to impress your customer base</li>
+									<li>Mobile applications for iPhone, Android, and/or Windows Phone</li>
+								</ul>
+								<p><strong>SPECIFIC TECHNOLOGIES: </strong></p>
+								<ul>
+								<li>WEB: <b>PHP, HTML5, CSS3, JavaScript, JQuery, ASP.NET</b></li>
+								<li>LANGUAGES:<b> C++, C#, Java</b></li>
+								<li>FRAMEWORKS:<b>CodeIgniter, Cocos2d, Twitter Bootstrap, Wordpress</b></li>
+								<li>TOOLS/IDE:<b> Visual Studio 2012, Git, SourceTree, SVN, Coda2, Dreamweaver CS5, PhotoShop CS5, Lightwave 3D, InkScape</b> </li>
+								<li>DATABASE:<b> MSSQL, MySQL</b></li>
+								<li>PLATFORMS:<b> Windows, Mac, Linux, iOS, Android, Windows Phone/8</b></li>
+								</ul>
+							</article>
+						</div>
+					</div>
+				</div>
+			</section>
+	      <!--Experiences-->
+	      <section class="row sec">
+	        <div class="col-md-12">
+	          <div class="row">
+	            <aside class="col-md-3 aside">
+	              <h2>What to Expect</h2>
+	            </aside>
+	            <div class="col-md-6">
+	              <article class="border-left content">
+			  		<h3>A Great Experience!</h3>
+		  			<p><i>Trust</i></p>
+		  			<p>We are a professional team with experience working with clients in a professional manner. This isn't our first picnic!
+		  				We are now working for contract jobs, as Saint has just completed her time with <a href = "www.vanitytrove.com">Vanity Trove</a>.
+		  				</p>
+		  			<p><i>Realistic Goals</i></p>
+		  			<p>Unfortunately, developers may often oversell themselves and underestimate the project. Development is no simple task,
+		  				and each project presents unique challenges to overcome. These things take time. We work with closely with our clients, so 
+		  				they have a reasonable expectation of the cost, energy, and time required for a project. We promise to complete the project as close to the timeline expectations as possible.
+		  			</p>
+		  			<p><i>Project Checkpoints</i></p>
+		  			<p>When we work on a project we divide it into checkpoints, so a large project is divided into several smaller ones. 
+		  				This allows for greater transparency and trust, and so that you don't need to pay for anything until you are sure the project is progressing as planned.
+		  			</p>
+		  			<p><i>Documented Changes</i></p>
+		  			<p>We keep track of our work and document our progress. You will be able to see exactly how the project is progressing. </p>
+		  			<p><i>Clear Code</i></p>
+		  			<p>We try to make our project to be as simple to use as possible. As the client, you should be able to access and use your website/application
+		  				, and if you wish to have other developers or update our code in the future, the code and documentation will be written as clearly and as dynamically as possible,
+		  				so that they can do so with ease</p>
+				  </article>
+	            </div>
+	            <div class="col-md-3">
+	              <h5 class="date" style="text-align:center;">No leaps of Faith Required</h5>
+	            </div>
+	          </div>
+	        </div>
+	      </section>
+	      <!--How to Hire Us-->
+		<section class="row sec">
+			<aside class="col-md-3 aside">
+				<h2>How To Hire Us</h2>
+			</aside>
+			<div class="col-md-6">
+				<article class="border-left content">
+					<h3>Shoot us an Email!</h3>
+					<p><i><a href="mailto:saintayechanhtoon@gmail.com">saintayechanhtoon@gmail.com</a></i></p>
+					<p>If you would like a quote, want to inquire, or just express an interest, just send us an email and we will get back to you quickly
+					In line with our goal of good communication, everything is commitment free until we construct specifications for the project, agree to a timeline,
+					and have complete understanding from both parties.	
+					</p>
+					<form role="form" action="email.php" method="post" >
+						<div class="form-group col-md-6">
+							<div class="form-group">
+						    	<input type="text" class="form-control" id="full_name" placeholder="Full Name">
+							</div>
+						</div>
+						<div class="form-group col-md-6">
+							<div class="form-group">
+						    	<input type="email" class="form-control" id="email" placeholder="Contact Email">
+							</div>
+						</div>
+						<div class="col-md-12">
+							<div class="form-group">
+						    	<textarea rows="4" class="form-control" id="message" placeholder="Message">
+						    	</textarea>
+							</div>
+						</div>
+					</form>
+				</article>
+			</div>
+			<div class="col-md-3">
+				<h5 class="date">Hire Us!</h5>
+			</div>
+		</section>
+		<section class="row sec">
+			<div class="col-md-12 footer">
+				<center><a href="#top" class="up" aria-hidden="true" data-icon="&#x3b;" title="Up?"></a></center>
+				<center><h5>&copy; Saint Aye Chan Htoon 2014 </h5></center>
+				<center><h6>Icon font by Icomoon. Responsive theme by Bootstrap 3.0.</h6></center> 
+			</div>
+			</section>
+	    </div>
+  </body>
+  <script src="./js/jquery-1.10.2.min.js"></script>
+  <script src="./js/bootstrap.min.js"></script>
+  <script src="./js/lightbox-2.6.min.js"></script>
+  <script>
+    $('a').click(function(){
+      $('html, body, div').animate({
+        scrollTop: $($(this).attr('href') ).offset().top
+        }, 500);
+        return false;
+    });
+  </script>
+  <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    
+      ga('create', 'UA-43291400-1', 'saintayechanhtoon.me');
+      ga('send', 'pageview');
+    </script>
+</html>
